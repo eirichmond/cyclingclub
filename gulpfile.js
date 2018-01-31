81883aecc0af18d1b286e9be1f180b840e6048b6
Elliott Richmond <elliott@squareonemd.co.uk>
2018-01-31 22:28:02 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('sass', function(){
+  return gulp.src('sass/style.scss')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(sass({outputStyle: 'expanded'})) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest(''))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+      proxy: 'dev.cyclingclubpro.local', // or project.dev/app/
+  })
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('sass/**/*.scss', ['sass']); 
+  // Other watchers
+});
